de3db27ce5d560b184a7e283dd4e430a9d2236a3
FinalF <flameyufeng@gmail.com>
2014-10-28 15:33:30 -0400

    update information in the info matrix in each time stamp

---
+++ mobility_analysis/RawDataReadIn.java
@@ -0,0 +1,76 @@
+package mobility_analysis;
+
+import general_stats.RawData_Process;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class RawDataReadIn {
+
+	/*read in the coordination of all the files
+	 * update the infomatrix
+	 * hashmap<nodename, nodeInfo>*/
+	File FileRawDataDic;
+	int lines=0; //define which line to read
+	
+	RawDataReadIn(String RawDataPath, int lines){
+		this.FileRawDataDic=new File(RawDataPath);
+		this.lines=lines;
+	}
+	
+	boolean infoUpdate(HashMap<String,nodeInfo> matrix) throws IOException{
+		for(File f: FileRawDataDic.listFiles()){
+			String nodename= f.getName();
+			BufferedReader in=new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+			String line=null;
+			int count=0;
+			while((line=in.readLine())!=null){
+				count++;
+				if(count==lines) break;
+			}
+			if(count<lines){
+				matrix.remove(nodename); //this node has left the cellular network
+			}else{
+				/*obtain the coordinates*/
+				line=line.trim();
+				String[] coordinates = line.split(" ");
+				int l=coordinates.length-1;
+				double[] tmp=new double[3];
+				int i=2;
+				while(l>=0){
+					if(!coordinates[l].isEmpty()){
+						tmp[i]=Double.parseDouble(coordinates[l]);
+						i--;
+					}
+					l--;
+				}
+				double t = tmp[0];
+				double x = tmp[1];
+				double y =tmp[2];
+				/*update the info matrix*/
+				if(matrix.containsKey(nodename)){
+					//already exists, update
+					nodeInfo ni=matrix.get(nodename);
+					ni.update(x, y);
+					matrix.put(nodename, ni);
+				}else{
+					//not exists, create
+					nodeInfo ni=new nodeInfo(x,y);
+					matrix.put(nodename, ni);					
+				}
+			}
+			
+		}
+		return true;
+	}
+	
+}
+
+
+
--- mobility_analysis/nodeInfo.java
+++ mobility_analysis/nodeInfo.java
@@ -4,10 +4,13 @@ public class nodeInfo {
 
 	/*Yufeng Fall2014*/
 	/*define the necessary info needs for each node
+	 * 0. coordination
 	 * 1. signal strength
 	 * 2. supported devices capacity in theory 
 	 * 3. actual capacity
 	 * 4. common interests shared with the GO*/
+	double x=0;
+	double y=0;
 	double ss=0;
 	int capacity=0;
 	int capacityActual=0;
@@ -18,15 +21,19 @@ public class nodeInfo {
 		
 	}
 	nodeInfo(double x, double y){
+		this.x=x;
+		this.y=y;
 		this.distanceSqua=Math.pow(x,2)+Math.pow(y, 2);
 		this.ssPara=Math.random()*distanceSqua+1; //random in [1,d^2]
 		this.ss=ssPara/distanceSqua;
-		this.capacity= (int) Math.random()*6;
+		this.capacity= (int) Math.random()*6+1;
 		this.capacityActual=(int) Math.random()*this.capacity;
 		this.commonI=Math.random();
 	}
 	
 	void update(double x, double y){
+		this.x=x;
+		this.y=y;
 		this.distanceSqua=Math.pow(x,2)+Math.pow(y, 2);
 		this.ssPara=Math.random()*distanceSqua+1; //random in [1,d^2]
 		this.ss=ssPara/distanceSqua;
